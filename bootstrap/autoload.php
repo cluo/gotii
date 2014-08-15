e4e8e802a87e57601ad6272b4c9855af1be98354
carlton <zstxt1989@163.com>
2014-08-15 14:30:58 +0800

    将composer的autoload包含放到autoload.php脚本中

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -5,9 +5,14 @@
  * @license http://opensource.org/licenses/mit-license.php
  */
 
+include ROOT_PATH."/vendor/autoload.php";//composer的自动加载器
+
+/*框架的自动加载规则*/
 $phalconLoader = new \Phalcon\Loader();
+
 $phalconLoader->registerNamespaces(array(
     "Controller" => APP_PATH."/controllers",
     "Model"=>APP_PATH."/models"
-));
+));//通过命名空间路径映射实现自动加载
+
 $phalconLoader->register();
