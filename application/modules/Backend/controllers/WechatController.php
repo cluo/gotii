d4c7b995c65844a8c3065185ddd552bf49ced61a
carlton <zstxt1989@163.com>
2014-08-25 17:28:04 +0800

    gitpush

--- application/modules/Backend/controllers/WechatController.php
+++ application/modules/Backend/controllers/WechatController.php
@@ -1,5 +1,6 @@
 <?php
 namespace Backend\Controller;
+use \Model\Wechat as Wechat;
 /**
  * 公众号管理控制器
  * @author 吾爱 <carlton.cheng@foxmail.com>
@@ -9,12 +10,25 @@ class WechatController extends BackendController
 {
     public function indexAction()
     {
+        $list = Wechat::find();
+        $this->view->setVar('list',$list);
         $this->view->setVar('title','公众号管理-');
     }
     
     public function addAction()
     {
         $this->view->setVar('title','添加公众号-');
+    }
+    
+    public function editAction()
+    {
+        $id = $this->request->getQuery('id','int','0');
+        $id && $info = Wechat::findFirst(array('id=?1','bind'=>array(1=>$id)));
+        if (empty($info)) {
+            echo $this->message->error('账号不存在');
+            return $this->view->disable();
+        }
+        xdebug_var_dump($info);
     }
     
     public function updateAction()
